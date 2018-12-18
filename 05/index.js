4882922d8787bc07364e1ab5105ea1883843e955
Sergey Borachuk <sergeyborachuk@gmail.com>
2018-12-18 14:25:19 +0200

    zero donwtime

---
+++ 05/index.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
+
+if (cluster.isMaster) {
+  console.log('this is the master process: ', process.pid)
+  for (let i=0; i<numCPUs; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', worker => {
+    console.log(`worker process ${process.pid} had died`)
+    // console.log(`only ${Object.keys(cluster.workers).length} remaining `)
+    console.log(`starting new worker`)
+    cluster.fork()
+  })
+
+} else {
+  console.log(`started a worker at ${process.pid}`)
+  http.createServer((req, res) => {
+    // const message = `worker ${process.pid}`
+    // console.log(message)
+    // res.end(message)
+    res.end(`process ${process.pid}`)
+    
+    if (req.url === '/kill') {
+      process.exit()
+    // } else {
+    } else if (req.url === '/') {
+      console.log(`serving form ${process.pid}`)
+      // console.log(`working on request ${process.pid}...`)
+    }
+  }).listen(3000)
+}
